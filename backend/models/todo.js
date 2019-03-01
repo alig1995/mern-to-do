d2da56ad5d08b4821cf24b101903702c8395ebbb
Sean Boyle <sean94@gmail.com>
2019-03-01 01:15:12 +0000

    Added the todo model and createed the CR functionallity-working on the update

---
+++ backend/models/todo.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let TodoSchema = new Schema({
+  todo_description: {
+    type: String
+  },
+  todo_responsible: {
+    type: String
+  },
+  todo_priority: {
+    type: String
+  },
+  todo_completed: {
+    type: Boolean
+  }
+});
+
+module.exports = Todo = mongoose.model("Todo", TodoSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,77 @@
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const server = express();
-const port = 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 
+let Todo = require("./models/todo");
+const port = 5000;
+const server = express();
+
+server.use(bodyParser.json());
+server.use(cors());
+
+//connect to mongo
+mongoose.connect("mongodb://localhost:27017/todos", { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connected to localhost mongo db");
+});
+todoRoutes.route("/").get((req, res) => {
+  Todo.find({}, (err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+todoRoutes.route("/:id").get((req, res) => {
+  let id = req.params.id;
+
+  Todo.findById(id, (err, todo) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
+});
+todoRoutes.route("/add").post((req, res) => {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: "todo added successfully" });
+    })
+    .catch(err => {
+      res.status(400).json("faild to create new task");
+    });
+});
+todoRoutes.route("/update/:id").post((req, res) => {
+  let id = req.params.id;
+
+  Todo.findById(id, (err, todo) => {
+    if (!todo) {
+      res.status(404).send("data not found");
+    } else {
+      todo.todo_description = req.body.todo_description;
+      todo.todo_responsible = req.body.todo_responsible;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_completed;
+
+      todo
+        .save()
+        .then(todo => {
+          res.json("todo updated");
+        })
+        .catch(err => {
+          res.status(400).send("Update failed");
+        });
+    }
+  });
+});
 server.get("/", (req, res) => res.send("Hello World!"));
 
-// server.get('/api/users', (req, res) => {
-//   MongoClient.connect('mongodb://localhost:27017/REACTCA2', function (err, client) {
-//     if (err) throw err
-
-//     const db = client.db('REACTCA2')
-
-//     db.collection('Users').find().toArray(function (err, result) {
-//       if (err) throw err
-
-//       res.send(result);
-//     })
-//   })
-// });
-server.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+server.use("/todos", todoRoutes);
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
